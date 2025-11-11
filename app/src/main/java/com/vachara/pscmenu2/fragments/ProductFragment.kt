4fca316981b66a09ef90a82cec0670a10ca6a4f6
Watchara Ketkaew <wk996110@gmail.com>
2025-11-11 10:00:38 +0700

    first commit

---
+++ app/src/main/java/com/vachara/pscmenu2/fragments/ProductFragment.kt
@@ -0,0 +1,21 @@
+package com.vachara.pscmenu2.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.vachara.pscmenu2.R
+
+
+class ProductFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_product, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/vachara/pscmenu2/fragments/ServiceFragment.kt
@@ -0,0 +1,23 @@
+package com.vachara.pscmenu2.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.vachara.pscmenu2.R
+
+
+class ServiceFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_service, container, false)
+    }
+
+
+}
